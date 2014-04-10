21acd92bca06d922640aade0d6d7b43449ca8708
swelly127 <jessicashu127@gmail.com>
2014-04-10 17:56:15 -0400

    fix warning for warmup 1

--- async/warmup.ml
+++ async/warmup.ml
@@ -1,6 +1,6 @@
 
 let fork d f1 f2 =
-  Deferred.both (d >>= f1) (d >>= f2); ()
+  ignore (Deferred.both (d >>= f1) (d >>= f2))
 
 let deferred_map l f =
   return (List.map (fun x -> match Deferred.peek (x >>= f) with |Some x -> x) l)
