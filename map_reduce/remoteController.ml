4fa938d5ebd6dd42b161a8c90fdf360f5e86cc76
swelly127 <jessicashu127@gmail.com>
2014-04-17 00:10:12 -0400

    misc changes that are wrong

--- map_reduce/remoteController.ml
+++ map_reduce/remoteController.ml
@@ -3,10 +3,35 @@ open Async.Std
 let addresses = ref []
 
 let init addrs =
-	List.map Tcp.to_host_and_port addrs
+	addresses := addrs
 
 module Make (Job : MapReduce.Job) = struct
+
+
+  let reduce (k, vs) =
+    Job.reduce (k, vs) >>= fun out -> return (k, out)
+
+  module C = Combiner.Make(Job)
+
   let map_reduce inputs =
-    failwith "Nowhere special."
+
+
+ Deffered.List.map addresses (fun x -> Tcp.connect (Tcp.to_host_and_port x))
+	>>= (fun lst ->
+  	Writer.write_line w Job.name;
+  	Writer.write_line w WorkerRequest.MapRequest(Job.input)
+	  Reader.read_line r >>= (fun x ->
+	   (match x with
+	     | `Eof -> ()
+	     | `Ok s -> );
+	   Socket.shutdown sock `Both;
+	   return ()))
+
+    Deferred.List.map ~how:`Parallel inputs ~Job:
+    		>>= fun (sock, r, w) -> WorkerRequest.send Job.inpit
+      >>| List.flatten
+      >>| C.combine
+      >>= fun l ->
+    Deferred.List.map l reduce
 
 end
