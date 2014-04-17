164b9c470957571c4ea75254669667a0c891879f
swelly127 <jessicashu127@gmail.com>
2014-04-17 00:35:33 -0400

    ta at ctb is awesome

--- map_reduce/remoteController.ml
+++ map_reduce/remoteController.ml
@@ -7,31 +7,39 @@ let init addrs =
 
 module Make (Job : MapReduce.Job) = struct
 
+	type worker = Reader t * Writer t
 
-  let reduce (k, vs) =
-    Job.reduce (k, vs) >>= fun out -> return (k, out)
+  	let reduce (k, vs) =
+    	Job.reduce (k, vs) >>= fun out -> return (k, out)
 
-  module C = Combiner.Make(Job)
+  	module C = Combiner.Make(Job)
 
-  let map_reduce inputs =
+  	let map_reduce inputs =
 
 
- Deffered.List.map addresses (fun x -> Tcp.connect (Tcp.to_host_and_port x))
-	>>= (fun lst ->
-  	Writer.write_line w Job.name;
-  	Writer.write_line w WorkerRequest.MapRequest(Job.input)
-	  Reader.read_line r >>= (fun x ->
-	   (match x with
-	     | `Eof -> ()
-	     | `Ok s -> );
-	   Socket.shutdown sock `Both;
-	   return ()))
+	Deferred.List.map addresses (fun x ->
+	 	try_with
+	 		(Tcp.connect (Tcp.to_host_and_port x)
+			 	>>= (fun lst ->
+			  	Writer.write_line w Job.name;
+			  	Writer.write_line w WorkerRequest.MapRequest(Job.input)
+				  Reader.read_line r >>= (fun x ->
+				   (match x with
+				     | `Eof -> ()
+				     | `Ok s -> );
+				   Socket.shutdown sock `Both;
+				   return ()))
 
-    Deferred.List.map ~how:`Parallel inputs ~Job:
-    		>>= fun (sock, r, w) -> WorkerRequest.send Job.inpit
-      >>| List.flatten
-      >>| C.combine
-      >>= fun l ->
-    Deferred.List.map l reduce
+			    Deferred.List.map ~how:`Parallel inputs ~Job:
+			    		>>= fun (sock, r, w) -> WorkerRequest.send Job.inpit
+			      >>| List.flatten
+			      >>| C.combine
+			      >>= fun l ->
+			    Deferred.List.map l reduce
+			)
+
+	 	if error then throw something
+	 	)
+
 
 end
