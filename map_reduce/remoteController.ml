b12c94bd0b2b0de308745e5018c89187b2dce5ba
swelly127 <jessicashu127@gmail.com>
2014-04-18 14:19:57 -0400

    remote controller compiles

--- map_reduce/remoteController.ml
+++ map_reduce/remoteController.ml
@@ -18,42 +18,36 @@ module Make (Job : MapReduce.Job) = struct
 
   type 'a worker = 'a Pipe.Reader.t * 'a Pipe.Writer.t
 
-  let reduce (k, vs) =
-    pop addresses
-    >>= (fun z -> Tcp.connect (Tcp.to_host_and_port (fst z) (snd z)))
-    >>= (fun (sock, r, w) ->
-        Request.send w (Request.ReduceRequest(k, vs));
-        Response.receive r
-        >>= (fun x ->
-          match x with
-            | `Ok(Response.JobFailed(s)) -> raise (ReduceFailed s)
-            | `Ok(Response.ReduceResult(out)) -> Socket.shutdown sock `Both; return (k, out)
+  let connect () = pop addresses >>= (fun x -> Tcp.connect (Tcp.to_host_and_port (fst x) (snd x)))
 
-            | `Eof -> raise (InfrastructureFailure))
-      )
+  let reduce (k, vs) =
+    try_with connect >>= function
+        | Core.Std.Error(exn) -> raise exn
+        | Core.Std.Ok(sock, r, w) ->
+          Request.send w (Request.ReduceRequest(k, vs));
+          Response.receive r
+          >>= (fun x ->
+            match x with
+              | `Ok(Response.JobFailed(s)) -> raise (ReduceFailed s)
+              | `Ok(Response.ReduceResult(out)) -> Socket.shutdown sock `Both; return (k, out)
+              | `Ok(Response.MapResult(lst)) -> failwith "wrong result"
+              | `Eof -> raise (InfrastructureFailure))
 
     module C = Combiner.Make(Job)
 
-    let handle_result res = match res with
-            | `Ok(Response.JobFailed(s)) -> raise (ReduceFailed s)
-            | `Ok(Response.ReduceResult(out)) -> Socket.shutdown sock `Both; return (k, out)
-            | `Ok(Response.MapResult(lst)) -> Socket.shutdown sock `Both; return lst
-            | `Eof -> raise (InfrastructureFailure))
-
-
     let map_reduce inputs =
       Deferred.List.map inputs
-        (fun x ->
-          pop addresses
-          >>= (fun z -> Tcp.connect (Tcp.to_host_and_port (fst z) (snd z)))
-          >>= (fun (sock, r, w) ->
+        (fun x -> try_with connect >>= function
+          | Core.Std.Error(exn) -> raise exn
+          | Core.Std.Ok(sock, r, w) ->
               Writer.write_line w Job.name;
               Request.send w (Request.MapRequest(x));
               Response.receive r >>= (fun result ->
                 match result with
                   | `Ok(Response.JobFailed(s)) -> raise (MapFailed s)
                   | `Ok(Response.MapResult(lst)) -> Socket.shutdown sock `Both; return lst
-                  | `Eof -> raise (InfrastructureFailure))))
+                  | `Ok(Response.ReduceResult(out)) -> failwith "wrong result"
+                  | `Eof -> raise (InfrastructureFailure)))
       >>| List.flatten
       >>| C.combine
       >>= fun l ->
